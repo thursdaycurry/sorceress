17c82ef96c712ff7e8a7121d8150ef5ecf331e58
thursdaycurry <thursdaycurry@gmail.com>
2024-02-19 15:10:25 +0900

    데이터 있는 국가만 반환 + NotFoundError 전달

--- src/energy/energy.service.ts
+++ src/energy/energy.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import * as papa from 'papaparse';
 import { CountryService } from 'src/country/country.service';
 import { GetFactDto } from 'src/country/dto/getFact.dto';
@@ -10,23 +10,39 @@ export class EnergyService {
   async getElecGenSource(getFactDto: GetFactDto): Promise<any> {
     const countryFacts = await this.countryService.getCountryFact(getFactDto);
 
-    const results = countryFacts.map((country) => {
-      return {
-        name: country['Government']['Country name']['conventional short form'],
-        sources: country['Energy']['Electricity generation sources'],
-      };
-    });
+    const filterSource = async (countryFacts) => {
+      const result = [];
+
+      for await (const country of countryFacts) {
+        if (!!country['Energy']['Electricity generation sources']) {
+          result.push({
+            name: country['Government']['Country name']['conventional short form'],
+            sources: country['Energy']['Electricity generation sources'],
+          });
+        }
+      }
+      return result;
+    };
+
+    const filteredCountrySource = await filterSource(countryFacts);
+
+    if (!filteredCountrySource.length) {
+      throw new NotFoundException(`No data available`);
+    }
+
     const convertToTable = (factObj) => {
       const result = { name: factObj.name.text };
 
       Object.keys(factObj.sources).forEach((sourceName) => {
         const sourceDetail = factObj.sources[sourceName]['text'].split('%')[0];
-        result[sourceName] = sourceDetail; // TODO: 클라이언트 데이터 가독성 위해 결과 수치에 % 추가 가능여부 고려
+        result[sourceName] = sourceDetail;
       });
       return result;
     };
 
-    return results.map(convertToTable);
+    console.log(filteredCountrySource);
+
+    return filteredCountrySource.map(convertToTable);
   }
 
   async getElecGenSourceCSV(getFactDto: GetFactDto): Promise<any> {
